c85883fd655ae855cefb47569b69f16e49154458
Gian Luca Corso <glcorso@tiscali.com>
2019-05-14 12:53:32 +0200

    commit iniziale

---
+++ Gemfile
@@ -0,0 +1,29 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+ruby RUBY_VERSION
+
+gem "decidim", "~> 0.17.0"
+gem "bootsnap", "~> 1.3"
+gem "puma", "~> 3.0"
+gem "uglifier", "~> 4.1"
+gem "faker", "~> 1.9"
+gem "figaro"
+gem "passenger"
+gem 'delayed_job_active_record'
+gem "daemons"
+gem "pg"
+
+group :development, :test do
+  gem "byebug", "~> 10.0", platform: :mri
+  gem "decidim-dev", "0.17.0"
+end
+
+group :development do
+  gem "letter_opener_web", "~> 1.3"
+  gem "listen", "~> 3.1"
+  gem "spring", "~> 2.0"
+  gem "spring-watcher-listen", "~> 2.0"
+  gem "web-console", "~> 3.5"
+end
---
+++ config/initializers/decidim.rb
@@ -0,0 +1,81 @@
+# frozen_string_literal: true
+
+Decidim.configure do |config|
+  config.application_name = "Formez Decidim"
+  config.mailer_sender = "noreply@formez.org"
+
+  # Change these lines to set your preferred locales
+  config.default_locale = :it
+  config.available_locales = [:it, :en, :ca, :es]
+
+  # Geocoder configuration
+  # config.geocoder = {
+  #   static_map_url: "https://image.maps.cit.api.here.com/mia/1.6/mapview",
+  #   here_app_id: Rails.application.secrets.geocoder[:here_app_id],
+  #   here_app_code: Rails.application.secrets.geocoder[:here_app_code]
+  # }
+
+  # Custom resource reference generator method
+  # config.reference_generator = lambda do |resource, component|
+  #   # Implement your custom method to generate resources references
+  #   "1234-#{resource.id}"
+  # end
+
+  # Currency unit
+  # config.currency_unit = "€"
+
+  # The number of reports which an object can receive before hiding it
+  # config.max_reports_before_hiding = 3
+
+  # Custom HTML Header snippets
+  #
+  # The most common use is to integrate third-party services that require some
+  # extra JavaScript or CSS. Also, you can use it to add extra meta tags to the
+  # HTML. Note that this will only be rendered in public pages, not in the admin
+  # section.
+  #
+  # Before enabling this you should ensure that any tracking that might be done
+  # is in accordance with the rules and regulations that apply to your
+  # environment and usage scenarios. This component also comes with the risk
+  # that an organization's administrator injects malicious scripts to spy on or
+  # take over user accounts.
+  #
+  config.enable_html_header_snippets = false
+
+  # SMS gateway configuration
+  #
+  # If you want to verify your users by sending a verification code via
+  # SMS you need to provide a SMS gateway service class.
+  #
+  # An example class would be something like:
+  #
+  # class MySMSGatewayService
+  #   attr_reader :mobile_phone_number, :code
+  #
+  #   def initialize(mobile_phone_number, code)
+  #     @mobile_phone_number = mobile_phone_number
+  #     @code = code
+  #   end
+  #
+  #   def deliver_code
+  #     # Actual code to deliver the code
+  #     true
+  #   end
+  # end
+  #
+  # config.sms_gateway_service = "MySMSGatewayService"
+
+  # Etherpad configuration
+  #
+  # Only needed if you want to have Etherpad integration with Decidim. See
+  # Decidim docs at docs/services/etherpad.md in order to set it up.
+  #
+  # config.etherpad = {
+  #   server: Rails.application.secrets.etherpad[:server],
+  #   api_key: Rails.application.secrets.etherpad[:api_key],
+  #   api_version: Rails.application.secrets.etherpad[:api_version]
+  # }
+end
+
+Rails.application.config.i18n.available_locales = Decidim.available_locales
+Rails.application.config.i18n.default_locale = Decidim.default_locale
