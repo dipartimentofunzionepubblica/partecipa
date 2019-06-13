f5603839d4f03f91554be12e9b27899248e313d6
Gian Luca Corso <gianlucacorso@tiscali.it>
2019-06-13 15:04:09 +0200

    inserita modifica per la registrazione senza newsletter.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,9 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+//= require jquery
 //= require rails-ujs
 //= require activestorage
 //= require_tree .
+//= require_self
 //= require decidim
