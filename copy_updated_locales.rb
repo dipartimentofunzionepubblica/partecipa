0f7464b98e828998149cc386a68d02a399ce3150
Gian Luca Corso <glcorso@tiscali.com>
2019-06-20 14:30:24 +0200

    modificato script copy_updated_locales.rb in modo da rendere possibile non parametrizzare i path relativi alle Gem e rinominare i file .yml già dentro le Gem con la data di oggi al secondo.

--- copy_updated_locales.rb
+++ copy_updated_locales.rb
@@ -1,58 +1,65 @@
 require 'fileutils'
+require File.expand_path('../config/environment', __FILE__)
 
 class CopyUpdatedLocales
-	GEM_FOLDER = "/home/decidim/.rbenv/versions/2.6.2/lib/ruby/gems/2.6.0/gems/"
-	LOCAL_REPO_FOLDER = "/home/decidim/decidim-app/tmp/decidim_repo/"
-	LAST_VERSION = "0.17.0"
-	DECIDIM_ENGINES =
-	[
-		"decidim-accountability",
-		#"decidim-admin",
-		"decidim-assemblies",
-		"decidim-blogs",
-		"decidim-budgets",
-		"decidim-comments",
-		#"decidim-core",
-		"decidim-debates",
-		"decidim-dev",
-		"decidim-forms",
-		"decidim-generators",
-		"decidim-meetings",
-		"decidim-pages",
-		"decidim-participatory_processes",
-		"decidim-proposals",
-		"decidim-sortitions",
-		"decidim-surveys",
-		"decidim-system",
-		"decidim-verifications"
-	 ]
-	 #"decidim-api",
-	LOCALES_RELATIVE_PATH = "/config/locales/"
+	GEM_FOLDER = Gem.dir + "/gems"
+	LOCAL_REPO_FOLDER = Rails.root.join("tmp", "decidim_repo") 
+	LAST_VERSION = "0.17.1"
+	LOCALES_RELATIVE_PATH = "config/locales"
+	YML_FILENAME = "it.yml"
 
 	def initialize
-		DECIDIM_ENGINES.each  do |engine|
-			rename_file(GEM_FOLDER + engine + "-" + LAST_VERSION + LOCALES_RELATIVE_PATH + "it.yml")
+		decidim_engines =
+		[
+			"decidim-accountability",
+			"decidim-admin",
+			"decidim-assemblies",
+			"decidim-blogs",
+			"decidim-budgets",
+			"decidim-comments",
+			"decidim-core",
+			"decidim-debates",
+			"decidim-dev",
+			"decidim-forms",
+			"decidim-generators",
+			"decidim-meetings",
+			"decidim-pages",
+			"decidim-participatory_processes",
+			"decidim-proposals",
+			"decidim-sortitions",
+			"decidim-surveys",
+			"decidim-system",
+			"decidim-verifications"
+		]
+		decidim_engines.each  do |engine|
+			result = rename_file(GEM_FOLDER + "/" + engine + "-" + LAST_VERSION + "/" + LOCALES_RELATIVE_PATH + "/" + YML_FILENAME)
+			if !result then
+				decidim_engines.delete(engine)
+			end
 		end
-		DECIDIM_ENGINES.each  do |engine|
-			copy_file(LOCAL_REPO_FOLDER + engine + LOCALES_RELATIVE_PATH + "it.yml",  GEM_FOLDER + engine + "-" + LAST_VERSION + LOCALES_RELATIVE_PATH + "it.yml")
+		decidim_engines.each  do |engine|
+			copy_file(LOCAL_REPO_FOLDER.to_s + "/" + engine + "/" + LOCALES_RELATIVE_PATH + "/" + YML_FILENAME,  GEM_FOLDER + "/" + engine + "-" + LAST_VERSION + "/" + LOCALES_RELATIVE_PATH + "/" + YML_FILENAME)
 		end
 	end
 
 	def rename_file(file)
 		begin
-			puts "path = " + file
-			File.rename(file, file + "_OLD")
+			puts "rename_file path: " + file
+			time = Time.now.to_formatted_s(:db).to_s.gsub(' ', '_')
+			File.rename(file, file + "_OLD_" + time)
+			return true
 		rescue Errno::ENOENT => e
-			puts file + " NOT FOUND"
+			puts "Error in renaming file " +  file + " NOT FOUND: removing engine from list. Message: " + e.message
+			return false
 		end
 	end
 
-	def copy_file(source_file, dest_folder)
+	def copy_file(source_file, dest_path)
 		begin
-			puts "path = " + dest_folder
-			FileUtils.cp(source_file, dest_folder)
+			puts "copy_file source_file: " + source_file.to_s + " destination path: " + dest_path
+			FileUtils.cp(source_file, dest_path)
 		rescue StandardError => e
-			puts e.message
+			puts "Error in copying file: " + source_file.to_s + " to destination path: " + dest_path + ". Message: " + e.message
 		end
 	end
 end
