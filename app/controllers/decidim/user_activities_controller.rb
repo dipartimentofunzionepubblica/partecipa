4c1d1760f48da733ff872ab58112f00514b77243
Gian Luca Corso <gianlucacorso@tiscali.it>
2019-07-01 09:29:48 +0200

    resa privata all'utente la sezione activity, all'interno del profilo personale

---
+++ app/controllers/decidim/user_activities_controller.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+module Decidim
+  # The controller to show all the last activities in a Decidim Organization.
+  class UserActivitiesController < Decidim::ApplicationController
+    include Paginable
+    include UserGroups
+
+    helper Decidim::ResourceHelper
+    helper_method :activities, :user
+	
+	def index
+      raise ActionController::RoutingError, "Not Found" if current_user != user
+	end
+
+    private
+
+    def user
+      @user ||= current_organization.users.find_by(nickname: params[:nickname])
+    end
+
+    def activities
+      @activities ||= paginate(
+        ActivitySearch.new(
+          organization: current_organization,
+          user: user,
+          resource_type: "all"
+        ).run
+      )
+    end
+  end
+end
