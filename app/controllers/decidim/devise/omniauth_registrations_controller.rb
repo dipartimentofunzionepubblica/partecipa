a927caf704aeaa43d7bc0116f1af753c0fe0787f
Gian Luca Corso <glcorso@tiscali.com>
2019-09-26 12:37:40 +0000

    inserito partial relativo al pulsante Entra con Spid direttamente nel template sessions/new, eliminando l'intervento precendente su _omniauth_buttons. Ripristinati nel user model tutti gli OmniAuth provider con l'aggiunta di spidauth. Inseriti valori dell'initializer spid-rails all'interno del application.yml e secret.yml. Da gestire il logout su Spid. Da semplificare secondo modello Helsinki la OmniAuth strategy.

--- app/controllers/decidim/devise/omniauth_registrations_controller.rb
+++ app/controllers/decidim/devise/omniauth_registrations_controller.rb
@@ -5,155 +5,99 @@ module Decidim
 			include FormFactory
 			include Decidim::DeviseControllers
 
-      def new
-        @form = form(OmniauthRegistrationForm).from_params(params[:user])
-      end
+		  def new
+			@form = form(OmniauthRegistrationForm).from_params(params[:user])
+		  end
 
-      def create
-        form_params = user_params_from_oauth_hash || params[:user]
+		  def create
+			form_params = user_params_from_oauth_hash || params[:user]
 
-        @form = form(OmniauthRegistrationForm).from_params(form_params)
-        @form.email ||= verified_email
+			@form = form(OmniauthRegistrationForm).from_params(form_params)
+			@form.email ||= verified_email
 
-        CreateOmniauthRegistration.call(@form, verified_email) do
-          on(:ok) do |user|
-            if user.active_for_authentication?
-              sign_in_and_redirect user, event: :authentication
-              set_flash_message :notice, :success, kind: @form.provider.capitalize
-            else
-              expire_data_after_sign_in!
-              redirect_to root_path
-              flash[:notice] = t("devise.registrations.signed_up_but_unconfirmed")
-            end
-          end
-
-          on(:invalid) do
-            set_flash_message :notice, :success, kind: @form.provider.capitalize
-            render :new
-          end
-
-          on(:error) do |user|
-            if user.errors[:email]
-              set_flash_message :alert, :failure, kind: @form.provider.capitalize, reason: t("decidim.devise.omniauth_registrations.create.email_already_exists")
-            end
-
-            render :new
-          end
-        end
-      end
-
-      def after_sign_in_path_for(user)
-        if !pending_redirect?(user) && first_login_and_not_authorized?(user)
-          decidim_verifications.authorizations_path
-        else
-          super
-        end
-      end
-
-      # Calling the `stored_location_for` method removes the key, so in order
-      # to check if there's any pending redirect after login I need to call
-      # this method and use the value to set a pending redirect. This is the
-      # only way to do this without checking the session directly.
-      def pending_redirect?(user)
-        store_location_for(user, stored_location_for(user))
-      end
-
-      def first_login_and_not_authorized?(user)
-        user.is_a?(User) && user.sign_in_count == 1 && Decidim::Verifications.workflows.any? && user.verifiable?
-      end
-
-      def action_missing(action_name)
-        return send(:create) if devise_mapping.omniauthable? && User.omniauth_providers.include?(action_name.to_sym)
-
-        raise AbstractController::ActionNotFound, "The action '#{action_name}' could not be found for Decidim::Devise::OmniauthCallbacksController"
-      end
-
-      private
-
-      def oauth_data
-        @oauth_data ||= oauth_hash.slice(:provider, :uid, :info)
-      end
-
-      # Private: Create form params from omniauth hash
-      # Since we are using trusted omniauth data we are generating a valid signature.
-      def user_params_from_oauth_hash
-        return nil if oauth_data.empty?
-
-        {
-          provider: oauth_data[:provider],
-          uid: oauth_data[:uid],
-          name: oauth_data[:info][:name],
-          nickname: oauth_data[:info][:nickname],
-          oauth_signature: OmniauthRegistrationForm.create_signature(oauth_data[:provider], oauth_data[:uid])#,
-          #raw_data: oauth_hash
-        }
-      end
-
-      def verified_email
-        @verified_email ||= oauth_data.dig(:info, :email)
-      end
-
-      def oauth_hash
-        raw_hash = request.env["omniauth.auth"]
-        return {} unless raw_hash
-
-        raw_hash.deep_symbolize_keys
-      end
-			
-			
-			
-			
-=begin		    
-			def action_missing(provider)
-				# Set up authentication/authorizations here, and distribute tasks
-				# that are provider specific to other methods, leaving only tasks
-				# that work across all providers in this method.
-				puts ">>>>>>>>>>>>>>>>ACTION MISSING!!!!<<<<<<<<<<<<<<<<<<<<<"
-			end
-		  
-		  
-  		    def passthru
-				puts ">>>>>>>>>>>>>>>>>>>PASSTHRU!!!!<<<<<<<<<<<<<<<<<<<<<"
-				super
-		    end
-
-		  
-		    def failure
-				puts ">>>>>>>>>>>>>>>>>>>>>FAILURE!!!!<<<<<<<<<<<<<<<<<<<<<"
-				super
-		    end
-
-		    def after_omniauth_failure_path_for(scope)
-				super(scope)
-		    end
-		 
-		    def spidauth
-				begin
-					puts ">>>>>>>>>>>>>>>>>>>>SPIDAUTH!!!!<<<<<<<<<<<<<<<<<<<<<"
-					puts "request.env['omniauth.auth'] = " + request.env["omniauth.auth"].inspect
-					# You need to implement the method below in your model (e.g. app/models/user.rb)
-					@user = DecidimUser.from_omniauth(request.env["omniauth.auth"])
-					if !@user.nil?
-						puts "@user.inspect = " + @user.inspect
-					else
-						puts "@user.nil"
-					end
-					if @user
-						sign_in_and_redirect @user, event: :authentication #this will throw if @user is not activated
-						set_flash_message(:notice, :success, kind: "Spid") if is_navigational_format?
-					else
-						redirect_to new_decidim_user_registration_url
-					end
-
-				rescue StandardError => e
-					puts "OmniauthCallbacksController.spidauth ==============>" + e.message
-					puts "Backtrace:\n\t#{e.backtrace.join("\n\t")}"
-					redirect_to root_url
+			CreateOmniauthRegistration.call(@form, verified_email) do
+			  on(:ok) do |user|
+				if user.active_for_authentication?
+				  sign_in_and_redirect user, event: :authentication
+				  set_flash_message :notice, :success, kind: @form.provider.capitalize
+				else
+				  expire_data_after_sign_in!
+				  redirect_to root_path
+				  flash[:notice] = t("devise.registrations.signed_up_but_unconfirmed")
 				end
+			  end
+
+			  on(:invalid) do
+				set_flash_message :notice, :success, kind: @form.provider.capitalize
+				render :new
+			  end
+
+			  on(:error) do |user|
+				if user.errors[:email]
+				  set_flash_message :alert, :failure, kind: @form.provider.capitalize, reason: t("decidim.devise.omniauth_registrations.create.email_already_exists")
+				end
+
+				render :new
+			  end
 			end
-=end
-		
-		
+		  end
+
+		  def after_sign_in_path_for(user)
+			if !pending_redirect?(user) && first_login_and_not_authorized?(user)
+			  decidim_verifications.authorizations_path
+			else
+			  super
+			end
+		  end
+
+		  # Calling the `stored_location_for` method removes the key, so in order
+		  # to check if there's any pending redirect after login I need to call
+		  # this method and use the value to set a pending redirect. This is the
+		  # only way to do this without checking the session directly.
+		  def pending_redirect?(user)
+			store_location_for(user, stored_location_for(user))
+		  end
+
+		  def first_login_and_not_authorized?(user)
+			user.is_a?(User) && user.sign_in_count == 1 && Decidim::Verifications.workflows.any? && user.verifiable?
+		  end
+
+		  def action_missing(action_name)
+			return send(:create) if devise_mapping.omniauthable? && User.omniauth_providers.include?(action_name.to_sym)
+
+			raise AbstractController::ActionNotFound, "The action '#{action_name}' could not be found for Decidim::Devise::OmniauthCallbacksController"
+		  end
+
+		  private
+
+		  def oauth_data
+			@oauth_data ||= oauth_hash.slice(:provider, :uid, :info)
+		  end
+
+		  # Private: Create form params from omniauth hash
+		  # Since we are using trusted omniauth data we are generating a valid signature.
+		  def user_params_from_oauth_hash
+			return nil if oauth_data.empty?
+
+			{
+			  provider: oauth_data[:provider],
+			  uid: oauth_data[:uid],
+			  name: oauth_data[:info][:name],
+			  nickname: oauth_data[:info][:nickname],
+			  oauth_signature: OmniauthRegistrationForm.create_signature(oauth_data[:provider], oauth_data[:uid])
+			}
+		  end
+
+		  def verified_email
+			@verified_email ||= oauth_data.dig(:info, :email)
+		  end
+
+		  def oauth_hash
+			raw_hash = request.env["omniauth.auth"]
+			return {} unless raw_hash
+
+			raw_hash.deep_symbolize_keys
+		  end
 		end
 	end
 end
--- config/initializers/spid-rails.rb
+++ config/initializers/spid-rails.rb
@@ -1,24 +1,25 @@
 # frozen_string_literal: true
 
 Spid.configure do |config|
-  config.hostname = 'http://test_spid_decidim'
+  config.hostname = Rails.application.secrets.spid_hostname
 
   config.idp_metadata_dir_path = Rails.root.join('config', 'idp_metadata')
   config.private_key_pem = File.read(Rails.root.join('lib', '.keys', 'private_key.pem'))
   config.certificate_pem = File.read(Rails.root.join('lib', '.keys', 'certificate.pem'))
 
-  config.metadata_path = '/spid/metadata.xml'
-  config.login_path = '/spid/login'
-  config.logout_path = '/spid/logout'
-  config.acs_path = '/spid/samlsso'
-  config.slo_path = '/spid/samlsso'
-  config.default_relay_state_path = '/users/auth/spidauth/callback'
+  config.metadata_path = Rails.application.secrets.spid_metadata_path
+  config.login_path = Rails.application.secrets.spid_login_path
   
-  config.digest_method = Spid::SHA256
-  config.signature_method = Spid::RSA_SHA256
-  config.acs_binding = Spid::BINDINGS_HTTP_POST
-  config.slo_binding = Spid::BINDINGS_HTTP_REDIRECT
+  config.logout_path = Rails.application.secrets.spid_logout_path
+  config.acs_path = Rails.application.secrets.spid_acs_path
+  config.slo_path = Rails.application.secrets.spid_slo_path
+  config.default_relay_state_path = Rails.application.secrets.spid_default_relay_state_path
+  
+  config.digest_method = Rails.application.secrets.spid_digest_method.constantize
+  config.signature_method = Rails.application.secrets.spid_signature_method.constantize
+  config.acs_binding = Rails.application.secrets.spid_acs_binding.constantize
+  config.slo_binding = Rails.application.secrets.spid_slo_binding.constantize
   config.attribute_services = [
-    { name: 'Decidim', fields: ['spid_code','email','name','family_name'] }
+    { name: 'Decidim', fields: ['spid_code','name','family_name'] }
   ]
 end
