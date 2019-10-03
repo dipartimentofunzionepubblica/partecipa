57f4bbd4f8b2c8e0253f5b9f55eaf406aed91534
Gian Luca Corso <glcorso@tiscali.com>
2019-10-03 11:05:34 +0000

    modificata Spidauth strategy per eliminare log in eccesso, modificato SessionsController per integrare la logout di Spid. Modificata il OmniAuthStrategiesController per rispristinare la create originale e le funzioni chiamate da essa.

--- lib/omniauth/strategies/spidauth.rb
+++ lib/omniauth/strategies/spidauth.rb
@@ -9,52 +9,39 @@ module OmniAuth
 		def request_phase
 			idp_param = request.params["idp_param"]
 			attribute_service_index_param = request.params["attribute_service_index_param"] || 0
-			redirect spid_login_path(idp_name: idp_param, authn_context: Spid::L1, attribute_service_index: attribute_service_index_param)
+			authn_context_param = request.params["authn_context_param_param"] || Spid::L1
+			redirect spid_login_path(idp_name: idp_param, authn_context: authn_context_param, attribute_service_index: attribute_service_index_param)
 		end
 
 		def callback_phase
-			begin
-				puts "session[:spid]['session_index'] = " + session[:spid]["session_index"].inspect
-				puts "session[:spid]['attributes'] = " + session[:spid]["attributes"].inspect
-				
-				if !session.nil? && !session[:spid]["session_index"].blank? && !session[:spid]['attributes'].blank?
-					@provider = session[:spid]['idp']
-					@uid = session[:spid]['attributes']['spid_code']
-					@email = session[:spid]['attributes']['email']
-					auth_hash 
-					super
-				elsif !session[:spid]['errors'].blank?
-					puts "session[:spid]['errors'] = "  + session[:spid]['errors'].inspect
-					@errors = session[:spid]['errors']
-					auth_hash 
-					super
-				end
-			rescue StandardError => e
-				puts "Spidauth.callback_phase ==============>" + e.message
-				puts "Backtrace:\n\t#{e.backtrace.join("\n\t")}"
+			if !session.nil? && !session[:spid]["session_index"].blank? && !session[:spid]['attributes'].blank?
+				@provider = session[:spid]['idp']
+				@uid = session[:spid]['attributes']['spid_code']
+				@email = session[:spid]['attributes']['email']
+				auth_hash 
+				super
+			elsif !session[:spid]['errors'].blank?
+				@errors = session[:spid]['errors']
+				auth_hash 
+				super
 			end
 		end
 	  
 		def auth_hash
-			begin
-				OmniAuth::Utils.deep_merge(super(), {
-				  'provider' => @provider,
-				  'uid' => @uid,
-				  'user_info' => {
-					'email'	   => @email
-				  },
-				  'extra' => {
-					'raw_info' => @errors 
-				  }
-				})
-			rescue StandardError => e
-				puts "Spidauth.auth_hash ==============>" + e.message
-				puts "Backtrace:\n\t#{e.backtrace.join("\n\t")}"
-			end
+			OmniAuth::Utils.deep_merge(super(), {
+			  'provider' => @provider,
+			  'uid' => @uid,
+			  'user_info' => {
+				'email'	   => @email
+			  },
+			  'extra' => {
+				'raw_info' => @errors 
+			  }
+			})
 		end
 		
 		def authorize_params
-			super.merge(idp_param: request.params["idp_param"], attribute_service_index_param: request.params["attribute_service_index_param"])
+			super.merge(idp_param: request.params["idp_param"], attribute_service_index_param: request.params["attribute_service_index_param"], authn_context_param: request.params["authn_context_param"])
 		end
     end
   end
