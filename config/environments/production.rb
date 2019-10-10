dfb0ce3e7aa5761b58b0c087b167d755de921b3d
Gian Luca Corso <gianlucacorso@tiscali.it>
2019-10-10 11:12:04 +0000

    modificato css in modo da cambiare i colori principali di header e footer e integrare scss

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,9 +20,12 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
+  
   #########DA TOGLIERE IN PRODUZIONE##########
   config.public_file_server.enabled = true #ENV['RAILS_SERVE_STATIC_FILES'].present?
   ############################################
+  
+  
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = Uglifier.new(:harmony => true)
   # config.assets.css_compressor = :sass
@@ -68,7 +71,7 @@ Rails.application.configure do
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
