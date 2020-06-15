55c707452df4588c13d2b3e582707c6b4c5e152a
Gian Luca Corso <gianlucacorso@tiscali.it>
2020-06-15 19:42:28 +0200

    integrata soluzione al bug riguardante il caricameto delle Surveys attraverso l'eliminazione delle n,1 queries.

---
+++ app/forms/decidim/forms/questionnaire_form.rb
@@ -0,0 +1,32 @@
+# frozen_string_literal: true
+
+module Decidim
+  module Forms
+    # This class holds a Form to answer a questionnaire from Decidim's public page.
+    class QuestionnaireForm < Decidim::Form
+      attribute :responses, Array[AnswerForm]
+      attribute :user_group_id, Integer
+
+      attribute :tos_agreement, Boolean
+
+      validates :tos_agreement, allow_nil: false, acceptance: true
+      validate :session_token_in_context
+
+      # Private: Create the responses from the questionnaire questions
+      #
+      # Returns nothing.
+      def map_model(model)
+        self.responses = model.questions.map do |question|
+          AnswerForm.from_model(Decidim::Forms::Answer.new(question: question))
+        end
+      end
+
+      def session_token_in_context
+        return if context&.session_token
+
+        errors.add(:tos_agreement, I18n.t("activemodel.errors.models.questionnaire.request_invalid"))
+      end
+    
+	end
+  end
+end
