df44272d8ab355a075c46eb6ab07bf85d04e5aee
partecipaformez <maintainer-partecipa@formez.it>
2021-09-07 14:28:56 +0200

    aggiornato a versione decidim 0.21.0

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -54,7 +54,7 @@ Rails.application.configure do
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
-  config.log_level = :debug
+  config.log_level = :error
 
   # Prepend all log lines with the following tags.
   config.log_tags = [:request_id]
@@ -106,4 +106,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  config.action_mailer.default_url_options = { host: 'partecipa.gov.it', protocol: 'https'}
+  config.action_mailer.asset_host = 'https://partecipa.gov.it'
 end
