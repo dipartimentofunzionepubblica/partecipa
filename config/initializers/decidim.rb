22b6e9a8aa771a704f9c16aa3d2fae4ec919f831
Gian Luca Corso <gianlucacorso@tiscali.it>
2022-05-17 10:24:00 +0200

    aggiunti file yml per spid, modificato decidim initializer, aggiunti file .keep sotto alberature spid

--- config/initializers/decidim.rb
+++ config/initializers/decidim.rb
@@ -2,22 +2,22 @@
 
 Decidim.configure do |config|
   # The name of the application
-  config.application_name = "My Application Name"
+  config.application_name = "ParteciPa"
 
   # The email that will be used as sender in all emails from Decidim
-  config.mailer_sender = "change-me@example.org"
+  config.mailer_sender = "noreply@formez.org"
 
   # Sets the list of available locales for the whole application.
   #
   # When an organization is created through the System area, system admins will
   # be able to choose the available languages for that organization. That list
   # of languages will be equal or a subset of the list in this file.
-  config.available_locales = [:en, :ca, :es]
+  config.available_locales = [:en, :it]
 
   # Sets the default locale for new organizations. When creating a new
   # organization from the System area, system admins will be able to overwrite
   # this value for that specific organization.
-  config.default_locale = :en
+  config.default_locale = :it
 
   # Restrict access to the system part with an authorized ip list.
   # You can use a single ip like ("1.2.3.4"), or an ip subnet like ("1.2.3.4/24")
@@ -30,16 +30,24 @@ Decidim.configure do |config|
   # config.content_processors = []
 
   # Whether SSL should be enabled or not.
-  # config.force_ssl = true
+  config.force_ssl = true
 
   # Map and Geocoder configuration
   #
-  # == HERE Maps ==
-  # config.maps = {
-  #   provider: :here,
-  #   api_key: Rails.application.secrets.maps[:api_key],
-  #   static: { url: "https://image.maps.ls.hereapi.com/mia/1.6/mapview" }
-  # }
+  # Geocoder configuration
+  if Rails.application.secrets.geocoder
+    Decidim.configure do |config|
+      config.maps = {
+        provider: :here,
+        api_key: Rails.application.secrets.geocoder[:here_api_key],
+        static: { url: 'https://image.maps.ls.hereapi.com/mia/1.6/mapview' }
+      }
+      config.geocoder = {
+       timeout: 5,
+       units: :km
+      }
+	end
+  end
   #
   # == OpenStreetMap (OSM) services ==
   # To use the OSM map service providers, you will need a service provider for
@@ -106,7 +114,7 @@ Decidim.configure do |config|
   # end
 
   # Currency unit
-  # config.currency_unit = "€"
+  config.currency_unit = "€"
 
   # Defines the quality of image uploads after processing. Image uploads are
   # processed by Decidim, this value helps reduce the size of the files.
