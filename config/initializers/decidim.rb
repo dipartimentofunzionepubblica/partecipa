643fd31d839d41b05195f7e597efcd39bfc9f58e
Gian Luca Corso <gianlucacorso@tiscali.it>
2022-10-21 16:39:23 +0200

    modificate conf per webpacker

--- config/initializers/decidim.rb
+++ config/initializers/decidim.rb
@@ -12,12 +12,12 @@ Decidim.configure do |config|
   # When an organization is created through the System area, system admins will
   # be able to choose the available languages for that organization. That list
   # of languages will be equal or a subset of the list in this file.
-  config.available_locales = [:en, :ca, :es]
+  config.available_locales = [:it, :en]
 
   # Sets the default locale for new organizations. When creating a new
   # organization from the System area, system admins will be able to overwrite
   # this value for that specific organization.
-  config.default_locale = :en
+  config.default_locale = :it
 
   # Restrict access to the system part with an authorized ip list.
   # You can use a single ip like ("1.2.3.4"), or an ip subnet like ("1.2.3.4/24")
