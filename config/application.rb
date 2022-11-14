14da554ddf6b7848afb57161f8c2665199fca35a
Gian Luca Corso <gianlucacorso@tiscali.it>
2022-11-14 11:39:47 +0100

    aggiornata application

--- config/application.rb
+++ config/application.rb
@@ -12,10 +12,10 @@ require "decidim/rails"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module DecidimDevelopmentApp
+module ParteciPa
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    #config.load_defaults 6.0
+    config.load_defaults 6.0
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
