7d85d9c4ec8ebbd1fae99121dfc6a29fe70c7529
Gian Luca Corso <gianlucacorso@tiscali.it>
2022-11-21 18:12:32 +0100

    modificati path js e aggiornata migration privacy

--- db/migrate/20221121165739_create_decidim_privacy_settings.decidim_privacy.rb
+++ db/migrate/20221121165739_create_decidim_privacy_settings.decidim_privacy.rb
@@ -1,4 +1,11 @@
 # This migration comes from decidim_privacy (originally 20220607103408)
+# Copyright (C) 2022 Formez PA
+# This program is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, version 3.
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU Affero General Public License for more details.
+# You should have received a copy of the GNU Affero General Public License along with this program. If not, see <https://www.gnu.org/licenses/>
+
+# Migrazione che genera la tabella sul database dei settings durante l'installazione
+
 class CreateDecidimPrivacySettings < ActiveRecord::Migration[6.0]
   def up
     create_table :decidim_privacy_settings do |t|
@@ -7,6 +14,7 @@ class CreateDecidimPrivacySettings < ActiveRecord::Migration[6.0]
       t.boolean :user_avatar, default: true
       t.boolean :user_search, default: true
       t.boolean :user_follow, default: true
+      t.boolean :user_message, default: true
       t.boolean :user_index, default: true
       t.boolean :user_public_page, default: true
     end
