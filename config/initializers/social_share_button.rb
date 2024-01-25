5150cb80e0a43bee7e86347f784c5b383174d96e
Gian Luca Corso <gianlucacorso@tiscali.it>
2024-01-25 10:51:16 +0100

    modificato per bug https://github.com/decidim/decidim/issues/8956

--- config/initializers/social_share_button.rb
+++ config/initializers/social_share_button.rb
@@ -3,6 +3,9 @@
 # Further information on how to configure the SocialShareButton gem can be
 # found here: https://github.com/huacnlee/social-share-button#configure
 #
+# Mofiicato per eliminare i bottoni social in relazione al bug https://github.com/decidim/decidim/issues/8956
 SocialShareButton.configure do |config|
-  config.allow_sites = %w(twitter facebook whatsapp_app whatsapp_web telegram)
+  config.allow_sites = []
+#%w(twitter facebook whatsapp_app whatsapp_web telegram)
 end
+
