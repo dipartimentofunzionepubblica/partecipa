c10b632bedba39ed62e544c637a409a65dab359d
Gian Luca Corso <gianlucacorso@tiscali.it>
2025-03-27 15:04:57 +0100

    aggiunta policy per rimozione log ActiveStorage

--- config/initializers/lograge.rb
+++ config/initializers/lograge.rb
@@ -8,4 +8,10 @@ Rails.application.configure do
   config.lograge.custom_options = lambda do |event|
     { time: Time.now }
   end
+  
+  config.lograge.ignore_actions = [
+    "ActiveStorage::DiskController#show",
+    "ActiveStorage::RepresentationsController#show",
+    "ActiveStorage::Representations::RedirectController#show"
+  ]
 end
