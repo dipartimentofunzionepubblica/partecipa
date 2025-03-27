686c3147f66ade09d0027724ec0665d296be7f6e
Gian Luca Corso <gianlucacorso@tiscali.it>
2025-03-27 14:34:38 +0100

    aggiunto timestamp log

--- config/initializers/lograge.rb
+++ config/initializers/lograge.rb
@@ -3,4 +3,9 @@
 # config/environments/production.rb
 Rails.application.configure do
   config.lograge.enabled = true
+  
+  # add time to lograge
+  config.lograge.custom_options = lambda do |event|
+    { time: Time.now }
+  end
 end
