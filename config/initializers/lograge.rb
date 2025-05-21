500122199bb00000f094efca97e5890ba7d8a4ec
Gian Luca Corso <gianlucacorso@tiscali.it>
2025-05-21 17:06:20 +0200

    lograge settings

--- config/initializers/lograge.rb
+++ config/initializers/lograge.rb
@@ -4,9 +4,11 @@
 Rails.application.configure do
   config.lograge.enabled = true
   
-  # add time to lograge
-  config.lograge.custom_options = lambda do |event|
-    { time: Time.now }
+  config.lograge.custom_payload do |controller|
+    {
+      user_id: controller.current_user.try(:id),
+      ip: controller.request.try(:ip)
+    }
   end
   
   config.lograge.ignore_actions = [
